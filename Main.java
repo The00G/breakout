7a27f0e6995741ac5f4645403efdb1132aca1b34
The00G <101090069+The00G@users.noreply.github.com>
2022-03-07 09:44:36 +0100

    classes creation

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-<<<<<<< HEAD
-        int a=35; 
-=======
         int a=45; 
->>>>>>> 6cf4a2cc97fcae78c58b350db17880d8349f904e
     }
 }
