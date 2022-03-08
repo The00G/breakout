521fb6914e66ba9361599e73b5359921f2898e16
The00G <101090069+The00G@users.noreply.github.com>
2022-03-08 11:39:59 +0100

    tentative d'interface graphique

--- Game.java
+++ Game.java
@@ -1,5 +1,35 @@
 import javax.swing.*;
+import java.util.*;
+import java.awt.Color;
 
 public class Game extends JFrame {
     
+    public ArrayList<Brick> bricks;
+    public Ball ball;
+    public Platform platform;
+
+    public JPanel main, field;
+
+    public Game() {
+        super("Breakout!");
+        this.setBounds(10, 10, 600, 600);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.main = new JPanel();
+        this.main.setBounds(0,0,this.getWidth(), this.getHeight());
+        this.main.setBackground(Color.green);
+        this.main.setLayout(null);
+
+        this.field = new JPanel();
+        this.main.setBounds(this.getWidth()/4, this.getHeight()/4, this.getWidth()*3/4, this.getHeight()*3/4);
+        this.main.setBackground(Color.red);
+
+        this.main.add(this.field);
+        this.add(main);
+
+        this.setVisible(true);
+    }
+
+
 }
