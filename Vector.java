11fbba8f78c588997c88d9b48efedfb61ad3088f
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-03-09 15:41:56 +0100

    Modif des commentaires
    Il existe une manière bien définie de commenter ses classes en java. Elles permettent au logiciels de mieux les interpréter et de mieux fonctionner après.

--- Vector.java
+++ Vector.java
@@ -1,28 +1,52 @@
+/**
+ * Class vector
+ */
 public class Vector {
-    
+
     public double x, y;
 
+    /**
+     * Creates a vector given cartesian coordinates
+     * 
+     * @param x
+     * @param y
+     */
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    //constructor with polar or cartesian coordinates
+    /**
+     * Creates a vector given polar coordinates
+     * Can also be used with cartesian coordinates, dictaded by the polar boolean
+     * 
+     * @param a
+     * @param b
+     * @param polar
+     */
     public Vector(double a, double b, boolean polar) {
-        if(polar){
-            this.x = a*Math.cos(b);
-            this.y = a*Math.sin(b);
+        if (polar) {
+            this.x = a * Math.cos(b);
+            this.y = a * Math.sin(b);
         } else {
             this.x = a;
             this.y = b;
         }
     }
 
+    /**
+     * Creates a vector using an array as imput
+     * 
+     * @param v
+     */
     public Vector(double[] v) {
         this.x = v[0];
         this.y = v[1];
     }
 
+    /**
+     * Creates an empty vector
+     */
     public Vector() {
         this.x = 0;
         this.y = 0;
@@ -38,34 +62,67 @@ public class Vector {
         this.y -= v.y;
     }
 
-    //multiply by a factor
+    /**
+     * Multiplies the vector by a given factor
+     * 
+     * @param f
+     */
     public void mult(double f) {
         this.x *= f;
         this.y *= f;
     }
 
-    //dot product
+    /**
+     * Gives back the dot product between this and a given vector
+     * 
+     * @param v
+     * @return double
+     */
     public double dot(Vector v) {
-        return this.x*v.x + this.y*v.y;
+        return this.x * v.x + this.y * v.y;
     }
 
-    //magnitude of the vector
+    /**
+     * Returns the magnitude of the vector
+     * 
+     * @return double
+     */
     public double mag() {
-        return Math.sqrt(this.x*this.x + this.y*this.y);
-    }
-    
-    public void normalize(){
-        this.mult(1/this.mag());
+        return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 
+    /**
+     * sets the length to this to 1
+     */
+    public void normalize() {
+        this.mult(1 / this.mag());
+    }
+
+    /**
+     * returns a vector with the same coordinates
+     * 
+     * @return Vector
+     */
     public Vector clone() {
         return new Vector(this.x, this.y);
     }
 
+    /**
+     * Returns true if this vector has the same components as a given vector
+     * 
+     * @param v
+     * @return boolean
+     */
     public boolean equals(Vector v) {
-        return this.x==v.x && this.y==v.y;
+        return this.x == v.x && this.y == v.y;
     }
 
+    /**
+     * Returns the vector as an array with the x component as element 0 and the y
+     * component as element 1
+     * 
+     * @return double[]
+     */
     public double[] toArray() {
         double[] a = new double[2];
         a[0] = this.x;
@@ -73,48 +130,57 @@ public class Vector {
         return a;
     }
 
+    /**
+     * Sets the magnitude of this vector at a given length
+     * 
+     * @param f
+     */
     public void setMag(double f) {
         this.normalize();
         this.mult(f);
     }
 
-    public double angle() {
+    /**
+     * Returns the angle made by the vector to the horizontal axis
+     * 
+     * @return double
+     */
+    public double getAngle() {
         return Math.atan2(this.y, this.x);
     }
 
     public void setAngle(double a) {
         double mag = this.mag();
-        this.x = mag*Math.cos(a);
-        this.y = mag*Math.sin(a);
+        this.x = mag * Math.cos(a);
+        this.y = mag * Math.sin(a);
     }
 
     public void rotate(double a) {
-        this.setAngle(a+this.angle());
+        this.setAngle(a + this.angle());
     }
 
     public String toString() {
         return "(" + this.x + "; " + this.y + ")";
     }
 
-
-    public static Vector add(Vector v1, Vector v2){
-        return new Vector(v1.x+v2.x, v1.y+v2.y);
+    public static Vector add(Vector v1, Vector v2) {
+        return new Vector(v1.x + v2.x, v1.y + v2.y);
     }
 
-    public static Vector sub(Vector v1, Vector v2){
-        return new Vector(v1.x-v2.x, v1.y-v2.y);
+    public static Vector sub(Vector v1, Vector v2) {
+        return new Vector(v1.x - v2.x, v1.y - v2.y);
     }
 
-    public static Vector mult(Vector v1, double f){
-        return new Vector(v1.x*f, v1.y*f);
+    public static Vector mult(Vector v1, double f) {
+        return new Vector(v1.x * f, v1.y * f);
     }
 
-    public static double dot(Vector v1, Vector v2){
-        return v1.x*v2.x + v1.y*v2.y;
+    public static double dot(Vector v1, Vector v2) {
+        return v1.x * v2.x + v1.y * v2.y;
     }
 
     public static double mag(Vector v) {
-        return Math.sqrt(v.x*v.x + v.y*v.y);
+        return Math.sqrt(v.x * v.x + v.y * v.y);
     }
 
     public static Vector clone(Vector v) {
