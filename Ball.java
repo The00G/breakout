be5997664dc9e99e3d309af26228f88bd7920c32
The00G <101090069+The00G@users.noreply.github.com>
2022-03-11 09:42:41 +0100

    constructor Ball and Platform

--- Ball.java
+++ Ball.java
@@ -2,6 +2,27 @@ public class Ball {
     
     public int size; // radius
     public Vector pos; // position of the center
-    public double speed; 
+    public double speed;
+    public Vector direction;
 
+    public Ball(Vector p, int s, double sp, Vector d){
+        this.pos = p;
+        this.size = s;
+        this.speed = sp;
+        this.direction = Vector.normalize(d);
+    }
+    
+    public Ball(Vector p, int s, double sp){
+        this.pos = p;
+        this.size = s;
+        this.speed = sp;
+        this.direction = new Vector(0,-1);
+    }
+
+    public Ball(double posx, double posy, int s, double sp){
+        this.pos = new Vector(posx, posy);
+        this.size = s;
+        this.speed = sp;
+        this.direction = new Vector(0,-1);
+    }
 }
--- Platform.java
+++ Platform.java
@@ -3,4 +3,14 @@ public class Platform {
     public Vector pos; //position of the center
     public Vector size;
 
+    public Platform(Vector p, Vector s) {
+        this.pos = p;
+        this.size = s;
+    }
+
+    public Platform(double posx, double posy, double width, double height) {
+        this.pos = new Vector(posx, posy);
+        this.size = new Vector(width, height);
+    }
+
 }
