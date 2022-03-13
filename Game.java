f2abe30af183524661d41f3fb045b9707c3728cd
The00G <101090069+The00G@users.noreply.github.com>
2022-03-13 13:58:06 +0100

    field paint

--- Game.java
+++ Game.java
@@ -3,6 +3,16 @@ import java.util.*;
 import java.awt.*;
 
 public class Game extends JFrame {
+
+    /**
+     * for the painting method see:
+     * the problem : http://www.herongyang.com/Swing/JFrame-Draw-Graphics-paint-on-Frame.html
+     * solution 1 :http://www.herongyang.com/Swing/JFrame-Draw-Graphics-paint-on-Component.html
+     * solution 2 (the one implemented) : http://www.herongyang.com/Swing/JFrame-Draw-Graphics-paint-on-Content-Pane.html
+     * 
+     * what the fuck is a class in a class? see:
+     * https://www.geeksforgeeks.org/nested-classes-java/#:~:text=In%20Java%2C%20it%20is%20possible,more%20readable%20and%20maintainable%20code.
+     */
     
     final public Vector FIELD_DEFAULT_SIZE = new Vector(500, 700);
     public double fieldScale;
@@ -18,19 +28,21 @@ public class Game extends JFrame {
         this.setBounds(10, 10, 250, 350);
         this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.setVisible(true);
+        this.setContentPane(new Painter());
     }
 
-    public void paint(Graphics g) {
-        g.setColor(Color.black);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    public class Painter extends JComponent {
+        public void paint(Graphics g) {
+            g.setColor(Color.black);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-        this.fieldScale = Math.min(this.getWidth()/this.FIELD_DEFAULT_SIZE.x, this.getHeight()/this.FIELD_DEFAULT_SIZE.y);
-        this.fieldSize = Vector.mult(this.FIELD_DEFAULT_SIZE, this.fieldScale);
-        this.fieldOrigin = new Vector((int)( (this.getWidth()-this.fieldSize.x)/2 ),(int)( (this.getHeight()-this.fieldSize.y)/2 ));
+            fieldScale = Math.min(this.getWidth()/FIELD_DEFAULT_SIZE.x, this.getHeight()/FIELD_DEFAULT_SIZE.y);
+            fieldSize = Vector.mult(FIELD_DEFAULT_SIZE, fieldScale);
+            fieldOrigin = new Vector((int)( (this.getWidth()-fieldSize.x)/2 ),(int)( (this.getHeight()-fieldSize.y)/2 ));
 
-        g.setColor(Color.white);
-        g.drawRect((int)this.fieldOrigin.x, (int)this.fieldOrigin.y, (int)this.fieldSize.x, (int)this.fieldSize.y);
+            g.setColor(Color.white);
+            g.drawRect((int)fieldOrigin.x, (int)fieldOrigin.y, (int)fieldSize.x, (int)fieldSize.y);
+        }
     }
 }
