3741a2d021de49c5b7bcf7ca5874c3446e97c428
val23waaaah <101105811+val23waaaah@users.noreply.github.com>
2022-03-14 09:54:08 +0100

    creation de plusieurs methodes dans a classe brick et remplissage nitial des bricks

--- Brick.java
+++ Brick.java
@@ -4,13 +4,15 @@ public class Brick {
     
     public Vector pos; //position of the center
     public Vector size;
-    public int totalLife, life;
+    public int totalLife, life; // if the brick is unbreakable, set life inferior to 0
     public Color color;
+    private Color [] colorList = {Color.white, Color.red, Color.magenta, Color.orange, Color.yellow, Color.green, Color.cyan, Color.blue, Color.pink}
 
     public Brick(double posx, double posy, double width, double height, int tl, Color c) {
         this.pos = new Vector(posx, posy);
         this.size = new Vector(width, height);
         this.totalLife = tl;
+        this.life= tl;
         this.color = c;
     }
 
@@ -18,6 +20,7 @@ public class Brick {
         this.pos = new Vector(posx, posy);
         this.size = new Vector(width, height);
         this.totalLife = tl;
+        this.life= tl;
         this.color = Color.white;
     }
 
@@ -25,6 +28,7 @@ public class Brick {
         this.pos = pos;
         this.size = size;
         this.totalLife = tl;
+        this.life= tl;
         this.color = c;
     }
 
@@ -32,7 +36,41 @@ public class Brick {
         this.pos = pos;
         this.size = size;
         this.totalLife = tl;
+        this.life= tl;
         this.color = Color.white;
+    }
+
+    /**
+     * changes the color of the brick according to it's life
+     */
+    public void setColorBrick (){
+        if (this.life<8 && this.life>0){
+            this.color = colorList [this.life];
+        } else if(this.life >7){
+            this.color = Color.pink;
+        } else {
+            this.color = Color.gray;
+        }
+    } 
+
+    /**
+     * tells if a brick is dead and retruns true if it's the case
+     * @return
+     */
+    public boolean isDead (){
+        if(this.life == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * decreases the life of the brick by one and changes the color according to it's new life
+     */
+    public void ballTouchesBrick (){
+        this.life--;
+        this.setColorBrick();
     }
 
 }
--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner14;
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
@@ -43,6 +44,18 @@ public class Game extends JFrame {
 
             g.setColor(Color.white);
             g.drawRect((int)fieldOrigin.x, (int)fieldOrigin.y, (int)fieldSize.x, (int)fieldSize.y);
+        }
+    }
+
+    /**
+     * fills the list of bricks of the game according to the length and height of the standart frame
+     */
+    public void createBricks (){
+        for ( int i = 0; i<8; i++){
+            for( int j = 0; j<15; j++){
+                Brick newBrick = new Brick ( j*(FIELD_DEFAULT_SIZE.x/15)+FIELD_DEFAULT_SIZE.x/20, i*(FIELD_DEFAULT_SIZE.y/(3*8))+FIELD_DEFAULT_SIZE.y/(3*10), FIELD_DEFAULT_SIZE.x/20, FIELD_DEFAULT_SIZE.y/10, i);
+                bricks.add(newBrick);
+            }
         }
     }
 }
