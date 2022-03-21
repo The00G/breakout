ad24e6fdf2456c414b07af5276c89035e9f44498
val23waaaah <101105811+val23waaaah@users.noreply.github.com>
2022-03-21 09:56:07 +0100

    modifications briques

--- Menu.java
+++ Menu.java
@@ -57,7 +57,7 @@ public class Menu extends JFrame implements ActionListener{
 
         JLabel aboutText = new JLabel();
         aboutText.setBounds(10,10,230,330);
-        aboutText.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus odio, lacinia et ornare sit amet, imperdiet vitae turpis. Vestibulum in dui vitae est interdum suscipit. Nullam nec velit porttitor, egestas ligula quis, ultricies nisi. Pellentesque aliquet, quam eu finibus viverra, odio ex posuere leo, quis accumsan quam nibh sit amet ante. Phasellus tempus dapibus risus nec rhoncus. Praesent id ante sed nisi viverra dignissim. Aliquam aliquam maximus iaculis. In a accumsan ipsum, at lobortis dolor.");
+        aboutText.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed efficitur est. Cras semper ornare elit at porttitor. Cras et semper elit. Suspendisse lobortis nisl vitae quam elementum tempor eu in nulla. Cras non vulputate libero, et pharetra ligula. Duis nec turpis purus. Etiam elementum, ante et laoreet mollis, elit purus bibendum velit, sit amet suscipit lectus velit a magna. Proin tempus pretium mauris ut ultrices. Donec pretium varius neque, ac condimentum ante volutpat et. Fusce dignissim tristique ligula, quis cursus elit rhoncus at. Cras mattis, est quis consequat congue, nunc massa sollicitudin ex, in elementum leo libero id lectus. Vivamus vitae iaculis urna. Duis vehicula tempor quam sagittis tincidunt. Aliquam vel ligula eget eros vehicula varius. Pellentesque ultrices enim et nisl euismod, in convallis eros sagittis. Curabitur pellentesque aliquam mauris at aliquet.");
         aboutPanel.add(aboutText);
 
         returnButton = new JButton("Return to menu");
