7f49b664b5dff8ff334478770f46f0db1024e31f
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-03-29 10:58:10 +0200

    Modif pendant les maths
    modifs du about et ajout du logo insa dans le menu principal

--- Menu.java
+++ Menu.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 
+import helpers.mathHelper;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
@@ -12,6 +14,7 @@ public class Menu extends JFrame implements ActionListener {
     JPanel aboutPanel;
     JButton returnButton;
     ImageIcon icon = new ImageIcon("breakout logo.png");
+    ImageIcon iconInsa = new ImageIcon("Logo Insa.png");
 
     int buttonWidth = 150;
     int buttonHeight = 50;
@@ -36,26 +39,34 @@ public class Menu extends JFrame implements ActionListener {
         rulesButton = new JButton("Règles du jeu");
         rulesButton.setBounds(mainPanel.getWidth() / 2 - buttonWidth - 10, 370 + vertOffset, buttonWidth, buttonHeight);
         rulesButton.addActionListener(this);
+        rulesButton.setBackground(Color.white);
         mainPanel.add(rulesButton);
 
         aboutButton = new JButton("A propos");
         aboutButton.setBounds(mainPanel.getWidth() / 2 + 10, 370 + vertOffset, buttonWidth, buttonHeight);
         aboutButton.addActionListener(this);
+        aboutButton.setBackground(Color.white);
         mainPanel.add(aboutButton);
 
         playButton = new JButton("Jouer");
         playButton.setBounds(mainPanel.getWidth() / 2 - buttonWidth / 2, 310 + vertOffset, buttonWidth, buttonHeight);
         playButton.addActionListener(this);
+        playButton.setBackground(Color.white);
         mainPanel.add(playButton);
 
         // JLabel titre = new JLabel();
         // titre.setBounds(75,30,100,50);
         // titre.setText("Breakout !");
 
-        JLabel image = new JLabel(icon, JLabel.CENTER);
-        image.setBounds(mainPanel.getWidth() / 2 - icon.getIconWidth() / 2, 50, icon.getIconWidth(),
+        JLabel imageBreakout = new JLabel(icon, JLabel.CENTER);
+        imageBreakout.setBounds(mainPanel.getWidth() / 2 - icon.getIconWidth() / 2, 50, icon.getIconWidth(),
                 icon.getIconHeight());
-        mainPanel.add(image);
+        mainPanel.add(imageBreakout);
+
+        JLabel imageInsa = new JLabel(iconInsa, JLabel.CENTER);
+        imageInsa.setBounds(mainPanel.getWidth() - iconInsa.getIconWidth() - 25,
+                mainPanel.getHeight() - iconInsa.getIconHeight() - 45, iconInsa.getIconWidth(), iconInsa.getIconHeight());
+        mainPanel.add(imageInsa);
 
         // About panel ------------------------------------------
 
@@ -64,6 +75,7 @@ public class Menu extends JFrame implements ActionListener {
         aboutPanel.setVisible(false);
         aboutPanel.setLayout(null);
         this.add(aboutPanel);
+        int endOfText = aboutPanel.getHeight() / 2;
 
         // JLabel aboutText = new JLabel();
         // aboutText.setBounds(0, 0, aboutPanel.getWidth(), 100);
@@ -81,12 +93,23 @@ public class Menu extends JFrame implements ActionListener {
         String[] aboutText = {
                 "Ce jeu a été développé dans le cadre du projet de fin d'année du cours Algorithmique et programmation 1 & 2 dispensé en première année de FIMI à l'INSA Lyon.",
                 "Il utilise les notions d'algorithmique et de programmation en Java découvertes tout au long de l'année mais également d'autres notions découvertes dans un cadre extra-scolaire.",
-                "", "------------------- Crédits ------------------", "",
+                "", "------------------- Crédits ------------------",
                 "2022 - Réutilisation et modification autorisée", "Dernière mise à jour le 02/06/2021",
-                "@Jeremy Banks - Développeur", "@Margaux Nagel - Développeur", "@Theo Gaigé - Développeur" , "@Valentin Minot - Développeur"};
+                "@Jeremy Banks - Développeur", "@Margaux Nagel - Développeur", "@Theo Gaigé - Développeur",
+                "@Valentin Minot - Développeur" };
+
+        JLabel[] labels = new JLabel[aboutText.length];
+        for (int i = 0; i < aboutText.length; i++) {
+            labels[i] = new JLabel();
+            labels[i].setBounds(15, (int) mathHelper.lerp(15, endOfText, i * 1 / ((double) aboutText.length)),
+                    aboutPanel.getWidth(), 25);
+            labels[i].setText(aboutText[i]);
+            aboutPanel.add(labels[i]);
+        }
 
         returnButton = new JButton("Return to menu");
         aboutPanel.add(returnButton);
+        returnButton.setBounds(15, endOfText, buttonWidth, buttonHeight);
         returnButton.addActionListener(this);
 
     }
@@ -94,8 +117,8 @@ public class Menu extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == playButton) {
             Game game = new Game();
-            game.play();
-            game.end();
+            // game.play();
+            // game.end();
         } else if (e.getSource() == aboutButton) {
             this.showAbout();
         } else if (e.getSource() == returnButton) {
