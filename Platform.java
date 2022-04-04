5ee990043caa0d7edd0d8461439106a3dfe5fc80
margauxnagel <101093138+margauxnagel@users.noreply.github.com>
2022-04-04 09:31:16 +0200

    suppr truc inutile platefrome

--- Platform.java
+++ Platform.java
@@ -49,29 +49,7 @@ public class Platform {
         this.pos.x = newPosX;
     }
 
-    /*public void move(int xMax) { // takes in parameter fieldSize.x
-        pos.x += dx; // updates the plateform position
-        if (pos.x - size.x / 2 <= 0) { 
-            pos.x = size.x / 2;
-        }
-
-        if (pos.x + size.x / 2 >= xMax) {
-            pos.x = xMax - size.x / 2;
-        }
-    }
-
-    public void move ( ){
-        Vector mouse=MousePosition();
-        pos.x += mouse.x; 
-        if (pos.x-size.x/2<=0){
-            pos.x=size.x/2;
-        }
-        double field=Game.fieldsize.x; 
-        if (pos.x + size.x/2>=field)
-            pos.x = field - size.x / 2;
-
-
-    }*/
+    
 
     public void KeyPressed(KeyEvent e) {
         int key = e.getKeyCode();
@@ -91,22 +69,6 @@ public class Platform {
         if (key == KeyEvent.VK_RIGHT) {// if the right arrow is pressed
             dx = 0;
         }
-    }
-
-
-    /**
-     * https://www.tabnine.com/code/query/java.awt@Robot@mouseMove+java.awt@Robot@%3Cinit%3E
-     * 
-     * @return Vector containing the x and y coordinates from the mouse
-     */
-    public Vector MousePosition (){
-        PointerInfo i = MouseInfo.getPointerInfo();
-        Point b = i.getLocation();
-        int x = (int) b.getX();
-        int y = (int) b.getY();
-        Vector position = new Vector (x,y); 
-        //position.sub(Game.FieldOrigin); 
-        return position;         
     }
 
 }
