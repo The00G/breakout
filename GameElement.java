3d3b45ec3e23b3318170e7bf291a91d3f2da3fb3
The00G <101090069+The00G@users.noreply.github.com>
2022-04-10 12:03:58 +0200

    comments gameElement

--- GameElement.java
+++ GameElement.java
@@ -2,22 +2,53 @@ import java.awt.*;
 
 public abstract class GameElement {
     
+    /**
+     * Vector representing the position this game element
+     */
     public Vector pos;
+
+    /**
+     * Vector representing the size this game element
+     */
     public Vector size;
+
+    /**
+     * Color of this game element
+     */
     public Color color;
 
+    /**
+     * Creates a game element with a given position, size and color
+     * 
+     * @param p vector representing the position of the game element
+     * @param s vector representing the size of the game element
+     * @param c color of the game element
+     */
     public GameElement(Vector p, Vector s, Color c){
         this.pos = p;
         this.size = s;
         this.color = c;
     }
 
+    /**
+     * Creates a game element with a given position and size
+     * Set the color of the game element to white
+     * @param p vector representing the position of the game element
+     * @param s vector representing the size of the game element
+     */
     public GameElement(Vector p, Vector s){
         this.pos = p;
         this.size = s;
         this.color = Color.white;
     }
 
+    /**
+     * paint the game element 
+     * 
+     * @param g the {@code Graphics} context in which to paint
+     * @param fieldOrigin the origin of the coordinate system
+     * @param fieldScale the scale to apply to coordinates
+     */
     public void paint(Graphics g, Vector fieldOrigin, double fieldScale) {
         g.setColor(this.color);
         g.fillRect( (int) (fieldOrigin.x + (this.pos.x - this.size.x) * fieldScale),
