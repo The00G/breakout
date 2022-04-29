a33315c1d782e8351824976c525d2eed76f5f3a9
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-04-30 00:13:19 +0100

    fini les commentaires dans la classe brick

--- src/Brick.java
+++ src/Brick.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.awt.*;
 
 public class Brick extends Obstacle {
@@ -6,19 +7,23 @@ public class Brick extends Obstacle {
     final private static int POINTS_BY_HIT = 20;
     /**
      * Bricks' color depending on life left
-     * <ul><li>the color in position 0 is the color for indestructible bricks
-     * <li>the colors in position 1 to 7 are the colors for the corresponding life level
-     * <li>the color in position 8 is the color for bricks with a life level of 8 or more</ul>
+     * <ul>
+     * <li>the color in position 0 is the color for indestructible bricks
+     * <li>the colors in position 1 to 7 are the colors for the corresponding life
+     * level
+     * <li>the color in position 8 is the color for bricks with a life level of 8 or
+     * more
+     * </ul>
      */
     private static Color[] colorList = { Color.gray,
-        Color.white,
-        new Color(185, 213, 146),
-        new Color(251, 240, 156),
-        new Color(241, 154, 123),
-        new Color(237, 103, 93),
-        new Color(235, 128, 175),
-        new Color(175, 109, 205),
-        new Color(72, 106, 216)
+            Color.white,
+            new Color(185, 213, 146),
+            new Color(251, 240, 156),
+            new Color(241, 154, 123),
+            new Color(237, 103, 93),
+            new Color(235, 128, 175),
+            new Color(175, 109, 205),
+            new Color(72, 106, 216)
     };
 
     /**
@@ -31,16 +36,19 @@ public class Brick extends Obstacle {
      */
     public int life;
 
+    /**
+     * The probability of a bonus item appearing upon the brick's destruction
+     */
     public double bonusChance = 0.1;
-    
+
     /**
      * Create a new brick with a given position, size and life
      * 
-     * @param posx      the x coordinate of the center of the brick
-     * @param posy      the y coordinate of the center of the brick
-     * @param width     the width of the brick
-     * @param height    the height of the brick
-     * @param tl        the total life of the brick
+     * @param posx   the x coordinate of the center of the brick
+     * @param posy   the y coordinate of the center of the brick
+     * @param width  the width of the brick
+     * @param height the height of the brick
+     * @param tl     the total life of the brick
      */
     public Brick(double posx, double posy, double width, double height, int tl) {
         super(new Vector(posx, posy), new Vector(width, height));
@@ -52,9 +60,9 @@ public class Brick extends Obstacle {
     /**
      * Create a new brick with a given position, size and life
      * 
-     * @param pos       the coordinates of the center of the brick
-     * @param size      the size of the brick
-     * @param tl        the total life of the brick
+     * @param pos  the coordinates of the center of the brick
+     * @param size the size of the brick
+     * @param tl   the total life of the brick
      */
     public Brick(Vector pos, Vector size, int tl) {
         super(pos, size);
@@ -90,27 +98,28 @@ public class Brick extends Obstacle {
     }
 
     /**
-     * Decreases the life of this brick by one and updates the color according to its new life
+     * Decreases the life of this brick by one and updates the color according to
+     * its new life
      */
     @Override
     public int hit() {
         super.hit();
-        if(this.life > 0){
+        if (this.life > 0) {
             this.life--;
             this.setColorBrick();
         }
         return POINTS_BY_HIT;
-        //System.out.println("brick touchée");
+        // System.out.println("brick touchée");
     }
 
     @Override
     public void paint(Graphics g, Vector fieldOrigin, double fieldScale) {
         super.paint(g, fieldOrigin, fieldScale);
         g.setColor(Color.black);
-        g.drawRect( (int) (fieldOrigin.x + (this.pos.x - this.size.x) * fieldScale),
-                    (int) (fieldOrigin.y + (this.pos.y - this.size.y) * fieldScale),
-                    (int) (2 * this.size.x * fieldScale),
-                    (int) (2 * this.size.y * fieldScale));
+        g.drawRect((int) (fieldOrigin.x + (this.pos.x - this.size.x) * fieldScale),
+                (int) (fieldOrigin.y + (this.pos.y - this.size.y) * fieldScale),
+                (int) (2 * this.size.x * fieldScale),
+                (int) (2 * this.size.y * fieldScale));
     }
 
 }
