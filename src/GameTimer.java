a491b0a8a635e92d824d98041a2f010bd5d9776f
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-04-30 00:16:36 +0100

    fini les commentaires dans gameTimer

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.awt.event.*;
 import java.util.LinkedList;
 
@@ -9,8 +10,6 @@ import java.awt.*;
  * GameTimer
  */
 public class GameTimer implements ActionListener {
-
-    // ceci est un commentaire inutile mais pas si inutile que ça
 
     /**
      * Game window linked with this game timer
@@ -45,8 +44,8 @@ public class GameTimer implements ActionListener {
     /**
      * Creates a new game timer with a given time interval and game window
      * 
-     * @param interval  time interval of the game timer in ms
-     * @param g         game window linked with the game timer
+     * @param interval time interval of the game timer in ms
+     * @param g        game window linked with the game timer
      */
     public GameTimer(int interval, Game g) {
         this.g = g;
@@ -61,15 +60,15 @@ public class GameTimer implements ActionListener {
 
         mouseScreenPosition = MouseInfo.getPointerInfo().getLocation();
         windowPosition = g.getLocation();
-        mousePosition.x = (mouseScreenPosition.getX() - windowPosition.getX() - g.fieldOrigin.x)/g.scale;
-        mousePosition.y = (mouseScreenPosition.getY() - windowPosition.getY() - g.fieldOrigin.y)/g.scale;
-        //System.out.println(mousePosition);
+        mousePosition.x = (mouseScreenPosition.getX() - windowPosition.getX() - g.fieldOrigin.x) / g.scale;
+        mousePosition.y = (mouseScreenPosition.getY() - windowPosition.getY() - g.fieldOrigin.y) / g.scale;
+        // System.out.println(mousePosition);
 
         g.platform.move(mousePosition);
 
-        for ( int i = 0; i< g.bonusItems.size(); i++) {
+        for (int i = 0; i < g.bonusItems.size(); i++) {
             g.bonusItems.get(i).move();
-            if (starTouchesPlatform(g.bonusItems.get(i), g.platform)){
+            if (starTouchesPlatform(g.bonusItems.get(i), g.platform)) {
                 Game.playSound("pongBonus.wav");
                 g.createBonus();
                 g.bonusItems.remove(i);
@@ -79,14 +78,17 @@ public class GameTimer implements ActionListener {
         g.removeDeadBricks();
         g.end();
         g.repaint();
-        
+
     }
 
+    /**
+     * Moves the balls and deletes the out of bounds ones
+     */
     public void updateBalls() {
         LinkedList<Ball> lostBalls = new LinkedList<Ball>();
-        for(Ball b : g.balls) {
+        for (Ball b : g.balls) {
             g.score.add(b.move(g.obstacles, dt));
-            if(b.pos.y > Game.FIELD_DEFAULT_SIZE.y+b.size.y) {
+            if (b.pos.y > Game.FIELD_DEFAULT_SIZE.y + b.size.y) {
                 lostBalls.add(b);
                 Game.playSound("pongLose.wav");
             }
@@ -95,15 +97,27 @@ public class GameTimer implements ActionListener {
         g.elements.removeAll(lostBalls);
     }
 
+    /**
+     * Stops the game, closes the window and display the end of game window
+     */
     public void stop() {
         t.stop();
         g.dispose();
         g.finalFrame();
     }
-    public boolean starTouchesPlatform (BonusItem star, Platform platf){
-        if (star.pos.x< (platf.pos.x + platf.size.x) && star.pos.x> (platf.pos.x - platf.size.x) && star.pos.y< (platf.pos.y + platf.size.y) && star.pos.y > (platf.pos.y - platf.size.y)){
+
+    /**
+     * Returns whether or not a star touches the platform
+     * 
+     * @param star  the bonus item
+     * @param platf the game platform
+     * @return true or false
+     */
+    public boolean starTouchesPlatform(BonusItem star, Platform platf) {
+        if (star.pos.x < (platf.pos.x + platf.size.x) && star.pos.x > (platf.pos.x - platf.size.x)
+                && star.pos.y < (platf.pos.y + platf.size.y) && star.pos.y > (platf.pos.y - platf.size.y)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
