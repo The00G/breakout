50985ec6437fad8142b39b5d0de092f36a26126c
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-04-30 00:38:43 +0100

    fini les commentaires de la classe obstacle

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,8 +1,9 @@
 package src;
+
 import java.awt.*;
 
-public abstract class Obstacle extends GameElement{
-    
+public abstract class Obstacle extends GameElement {
+
     /**
      * Creates an obstacle with a given position, size and color
      * 
@@ -10,7 +11,7 @@ public abstract class Obstacle extends GameElement{
      * @param s vector representing the size of the obstacle
      * @param c color of the obstacle
      */
-    public Obstacle(Vector p, Vector s, Color c){
+    public Obstacle(Vector p, Vector s, Color c) {
         super(p, s, c);
     }
 
@@ -20,14 +21,15 @@ public abstract class Obstacle extends GameElement{
      * @param p vector representing the position of the obstacle
      * @param s vector representing the size of the obstacle
      */
-    public Obstacle(Vector p, Vector s){
+    public Obstacle(Vector p, Vector s) {
         super(p, s);
     }
 
     /**
      * Returns the normal vector of the surface portion nearest to a given point
-     * @param   p   
-     * @return  the normal vector of the surface portion nearest to a given point
+     * 
+     * @param p
+     * @return the normal vector of the surface portion nearest to a given point
      */
     public Vector normalVectorTo(Vector p) {
         double dx, dy;
@@ -45,18 +47,25 @@ public abstract class Obstacle extends GameElement{
         return Vector.normalized(new Vector(dx, dy));
     }
 
+    /**
+     * Returns the precise distance between a point and the closest point of the
+     * edge to said point, similar to ray tracing
+     * 
+     * @param p the point with which to calculate the distance
+     * @return the distance to the point
+     */
     public double distanceTo(Vector p) {
         double dx, dy;
         dx = Math.max(0, Math.max(this.pos.x - this.size.x - p.x, p.x - (this.pos.x + this.size.x)));
         dy = Math.max(0, Math.max(this.pos.y - this.size.y - p.y, p.y - (this.pos.y + this.size.y)));
 
-        return Math.sqrt(dx*dx + dy*dy);
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     /**
      * Signals to this obstacle that it has been hit
      * 
-     * @return  the points given when hitting the obstacle
+     * @return the points given when hitting the obstacle
      */
 
     public int hit() {
@@ -68,9 +77,9 @@ public abstract class Obstacle extends GameElement{
     /**
      * Update the direction of a ball to simulate a bounce on this obstacle
      * 
-     * @param bPos  the position of the ball
-     * @param bDir  the direction of the ball
-     * @return      the points made by the bounce
+     * @param bPos the position of the ball
+     * @param bDir the direction of the ball
+     * @return the points made by the bounce
      */
     public int bounce(Vector bPos, Vector bDir) {
         Vector normalVector = Vector.normalized(this.normalVectorTo(bPos));
