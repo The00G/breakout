f8da657840975663981361fea8ed0cea20fcc52d
JeyBanks <83754354+JeyBanks@users.noreply.github.com>
2022-04-30 00:20:27 +0100

    fini les commentaires dans la classe Level

--- src/Level.java
+++ src/Level.java
@@ -1,108 +1,119 @@
 package src;
+
 import java.util.LinkedList;
-import java.lang.Math; 
+import java.lang.Math;
 
 import helpers.mathHelper;
 
 public class Level {
-    
+
     /**
      * Create the bricks of a given level
      * 
      * @param id        Level id
      * @param fieldSize game's fieldsize
-     * @return          level's bricks
+     * @return level's bricks
      */
 
-    public static int getLevel(){
-        int [] levelTab = {1,2,3,4,5,55};
-        int randomlevel =levelTab[(int) (Math.random()*(levelTab.length))]; 
-        return randomlevel; 
+    /**
+     * @return a random level id out of the predefined ones
+     */
+    public static int getLevel() {
+        int[] levelTab = { 1, 2, 3, 4, 5, 55 };
+        int randomlevel = levelTab[(int) (Math.random() * (levelTab.length))];
+        return randomlevel;
     }
 
+    /**
+     * Places the bricks for the given level id
+     * 
+     * @param id        the wanted level
+     * @param fieldSize the size of the window
+     * @return the Brick {@coee LinkedList} containing the current bricks
+     */
     public static LinkedList<Brick> buildLevel(int id, Vector fieldSize) {
-        switch(id) {
+        switch (id) {
 
             case 1:
-            int[][] map1 = {{8,8,8,8,8,8,8,8,8,8},
-                            {7,7,7,7,7,7,7,7,7,7},
-                            {6,6,6,6,6,6,6,6,6,6},
-                            {5,5,5,5,5,5,5,5,5,5},
-                            {4,4,4,4,4,4,4,4,4,4},
-                            {3,3,3,3,3,3,3,3,3,3},
-                            {2,2,2,2,2,2,2,2,2,2},
-                            {1,1,1,1,1,1,1,1,1,1}};
-            return buildBricks(map1, fieldSize);
+                int[][] map1 = { { 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 },
+                        { 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 },
+                        { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+                        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+                        { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+                        { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+                        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+                return buildBricks(map1, fieldSize);
 
             case 2:
-            int[][] map2 = {{8,0,8,0,8,0,8,0,8,0},
-                            {0,7,0,7,0,7,0,7,0,7},
-                            {6,0,6,0,6,0,6,0,6,0},
-                            {0,5,0,5,0,5,0,5,0,5},
-                            {4,0,4,0,4,0,4,0,4,0},
-                            {0,3,0,3,0,3,0,3,0,3},
-                            {2,0,2,0,2,0,2,0,2,0},
-                            {0,1,0,1,0,1,0,1,0,1}};
-            return buildBricks(map2, fieldSize);
+                int[][] map2 = { { 8, 0, 8, 0, 8, 0, 8, 0, 8, 0 },
+                        { 0, 7, 0, 7, 0, 7, 0, 7, 0, 7 },
+                        { 6, 0, 6, 0, 6, 0, 6, 0, 6, 0 },
+                        { 0, 5, 0, 5, 0, 5, 0, 5, 0, 5 },
+                        { 4, 0, 4, 0, 4, 0, 4, 0, 4, 0 },
+                        { 0, 3, 0, 3, 0, 3, 0, 3, 0, 3 },
+                        { 2, 0, 2, 0, 2, 0, 2, 0, 2, 0 },
+                        { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 } };
+                return buildBricks(map2, fieldSize);
 
             case 3:
-            int[][] map3 = {{8,8,8,8,8,8,8,8},
-                            {7,7,7,7,7,7,7},
-                            {6,6,6,6,6,6},
-                            {5,5,5,5,5},
-                            {4,4,4,4},
-                            {3,3,3},
-                            {2,2},
-                            {1}};
-            return buildBricks(map3, fieldSize);
+                int[][] map3 = { { 8, 8, 8, 8, 8, 8, 8, 8 },
+                        { 7, 7, 7, 7, 7, 7, 7 },
+                        { 6, 6, 6, 6, 6, 6 },
+                        { 5, 5, 5, 5, 5 },
+                        { 4, 4, 4, 4 },
+                        { 3, 3, 3 },
+                        { 2, 2 },
+                        { 1 } };
+                return buildBricks(map3, fieldSize);
 
             case 4:
-            int[][] map4 = {{0,7,0,7,0,7,0,7,0,7},
-                            {0,0,0,0,0,0,0,0,0,0},
-                            {0,5,0,5,0,5,0,5,0,5},
-                            {0,0,0,0,0,0,0,0,0,0},
-                            {0,3,0,3,0,3,0,3,0,3},
-                            {0,0,0,0,0,0,0,0,0,0},
-                            {0,1,0,1,0,1,0,1,0,1}};
-            return buildBricks(map4, fieldSize);
+                int[][] map4 = { { 0, 7, 0, 7, 0, 7, 0, 7, 0, 7 },
+                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                        { 0, 5, 0, 5, 0, 5, 0, 5, 0, 5 },
+                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                        { 0, 3, 0, 3, 0, 3, 0, 3, 0, 3 },
+                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                        { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 } };
+                return buildBricks(map4, fieldSize);
 
             case 5:
-            int[][] map5m = {{7,7,7,7,7,7,7,7,7,7},
-                             {6,6,6,6,6,6,6,6,6,6},
-                             {5,5,5,5,5,5,5,5,5,5},
-                             {4,4,4,4,4,4,4,4,4,4},
-                             {3,3,3,3,3,3,3,3,3,3},
-                             {2,2,2,2,2,2,2,2,2,2},
-                             {1,1,1,1,1,1,1,1,1,1},
-                             {0,0,0,0,0,0,0,0,0,0}};
-            int[][] map5M = {{8,8,8,8,8,8,8,8,8,8},
-                             {8,8,8,8,8,8,8,8,8,8},
-                             {7,7,7,7,7,7,7,7,7,7},
-                             {6,6,6,6,6,6,6,6,6,6},
-                             {5,5,5,5,5,5,5,5,5,5},
-                             {4,4,4,4,4,4,4,4,4,4},
-                             {3,3,3,3,3,3,3,3,3,3},
-                             {2,2,2,2,2,2,2,2,2,2}};
-            return buildBricks(map5m, map5M, fieldSize);
+                int[][] map5m = { { 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 },
+                        { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+                        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+                        { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+                        { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+                        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+                        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+                int[][] map5M = { { 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 },
+                        { 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 },
+                        { 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 },
+                        { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+                        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+                        { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+                        { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+                        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 } };
+                return buildBricks(map5m, map5M, fieldSize);
 
             case 55:
-            int[][] map55 = {   { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-                                { 5,-1,-1,-1,-1,-1, 5,-1,-1,-1,-1,-1, 5},
-                                { 5,-1, 5, 5, 5, 5, 5,-1, 5, 5, 5, 5, 5},
-                                { 5,-1,-1,-1,-1, 5, 5,-1,-1,-1,-1, 5, 5},
-                                { 5, 5, 5, 5, 5,-1, 5, 5, 5, 5, 5,-1, 5},
-                                { 5, 5, 5, 5, 5,-1, 5, 5, 5, 5, 5,-1, 5},
-                                { 5,-1, 5, 5, 5,-1, 5,-1, 5, 5, 5,-1, 5},
-                                { 5, 5,-1,-1,-1, 5, 5, 5,-1,-1,-1, 5, 5},
-                                { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}};
-            return buildBricks(map55, fieldSize);
-            
+                int[][] map55 = { { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+                        { 5, -1, -1, -1, -1, -1, 5, -1, -1, -1, -1, -1, 5 },
+                        { 5, -1, 5, 5, 5, 5, 5, -1, 5, 5, 5, 5, 5 },
+                        { 5, -1, -1, -1, -1, 5, 5, -1, -1, -1, -1, 5, 5 },
+                        { 5, 5, 5, 5, 5, -1, 5, 5, 5, 5, 5, -1, 5 },
+                        { 5, 5, 5, 5, 5, -1, 5, 5, 5, 5, 5, -1, 5 },
+                        { 5, -1, 5, 5, 5, -1, 5, -1, 5, 5, 5, -1, 5 },
+                        { 5, 5, -1, -1, -1, 5, 5, 5, -1, -1, -1, 5, 5 },
+                        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } };
+                return buildBricks(map55, fieldSize);
+
             case -1:
-            int [][] map404 = {{1,0},{0,0}};
-            return buildBricks(map404, fieldSize);
+                int[][] map404 = { { 1, 0 }, { 0, 0 } };
+                return buildBricks(map404, fieldSize);
 
             default:
-            return new LinkedList<>();
+                return new LinkedList<>();
         }
     }
 
@@ -111,15 +122,16 @@ public class Level {
      * 
      * @param map       map containing the total life of the bricks
      * @param fieldSize game's fieldsize
-     * @return          list of bricks placed according to the map
+     * @return list of bricks placed according to the map
      */
     public static LinkedList<Brick> buildBricks(int[][] map, Vector fieldSize) {
         LinkedList<Brick> bricks = new LinkedList<Brick>();
-        for(int i = 0; i < map.length; i++){
-            Vector brickSize = new Vector(fieldSize.x/(2*map[i].length),fieldSize.y/(6*map.length));
-            for(int j = 0; j < map[i].length; j++) {
-                if (map[i][j] !=0 ) {
-                    bricks.add(new Brick(new Vector((2*j+1)*brickSize.x,(2*i+3)*brickSize.y),brickSize,map[i][j]));
+        for (int i = 0; i < map.length; i++) {
+            Vector brickSize = new Vector(fieldSize.x / (2 * map[i].length), fieldSize.y / (6 * map.length));
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] != 0) {
+                    bricks.add(new Brick(new Vector((2 * j + 1) * brickSize.x, (2 * i + 3) * brickSize.y), brickSize,
+                            map[i][j]));
                 }
             }
         }
@@ -133,16 +145,17 @@ public class Level {
      * @param mapMin    map containing the minimum total life of the bricks
      * @param mapMax    map containing the maximum total life of the bricks
      * @param fieldSize game's fieldsize
-     * @return          list of bricks placed according to the map
+     * @return list of bricks placed according to the map
      */
     public static LinkedList<Brick> buildBricks(int[][] mapMin, int[][] mapMax, Vector fieldSize) {
         LinkedList<Brick> bricks = new LinkedList<Brick>();
-        for(int i = 0; i < mapMin.length; i++){
-            Vector brickSize = new Vector(fieldSize.x/(2*mapMin[i].length),fieldSize.y/(6*mapMin.length));
-            for(int j = 0; j < mapMin[i].length; j++) {
-                int life = mathHelper.RandomIntBetween(mapMin[i][j],mapMax[i][j]);
-                if (life !=0 ) {
-                    bricks.add(new Brick(new Vector((2*j+1)*brickSize.x,(2*i+3)*brickSize.y),brickSize,life));
+        for (int i = 0; i < mapMin.length; i++) {
+            Vector brickSize = new Vector(fieldSize.x / (2 * mapMin[i].length), fieldSize.y / (6 * mapMin.length));
+            for (int j = 0; j < mapMin[i].length; j++) {
+                int life = mathHelper.RandomIntBetween(mapMin[i][j], mapMax[i][j]);
+                if (life != 0) {
+                    bricks.add(new Brick(new Vector((2 * j + 1) * brickSize.x, (2 * i + 3) * brickSize.y), brickSize,
+                            life));
                 }
             }
         }
